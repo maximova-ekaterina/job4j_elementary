c43a7a95e7836d21753021a3db983611b4ec504b
maximova-ekaterina <skoropeda@yandex.ru>
2021-02-25 20:13:19 +0300

    4.1. Аргументы [#480494]

---
+++ src/ru/job4j/ArgMethod.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class ArgMethod {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+        System.out.println("Petr Arsentev age" + age);
+        ArgMethod.hello(name);
+
+
+    }
+}
