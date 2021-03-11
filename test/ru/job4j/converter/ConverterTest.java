c34e2954a5bd9dc09cd1990a44d5ea7fb45c92dd
maximova-ekaterina <skoropeda@yandex.ru>
2021-03-11 21:23:23 +0300

    1. Модульные тесты. [#480516]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140Rbl2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        int in = 120;
+        int expected = 2;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}
