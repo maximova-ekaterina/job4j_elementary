9d243cc70d2b95abe9803896d967c6918dd18cfc
maximova-ekaterina <skoropeda@yandex.ru>
2021-03-12 18:34:22 +0300

    0. Операции сравнения [#480431]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+    int first = 10;
+    int second = 9;
+
+    boolean resultOne = first > second;
+            System.out.println(resultOne);
+
+    boolean resultTwo = first < second;
+            System.out.println(resultTwo);
+
+    boolean resultThree = first == second;
+            System.out.println(resultThree);
+}
+}
