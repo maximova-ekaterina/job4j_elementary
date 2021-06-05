3b6258712c01f32a886c2d5c627497983667f0e3
maximova-ekaterina <skoropeda@yandex.ru>
2021-06-05 12:00:17 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -7,6 +7,17 @@ public class FindLoop {
             if (data[index] == el) {
                 rst = index;
                 break;
+           }
+        }
+        return rst;
+    }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = start; index < finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
             }
         }
         return rst;
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -3,12 +3,11 @@ package ru.job4j.array;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHas5Then0() {
-        int[] data = {5, 4, 3, 2};
+    public void whenArrayHasLength5Then0() {
+        int[] data = new int[] {5, 10, 3};
         int el = 5;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
@@ -16,20 +15,25 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenArrayHas6Then1() {
-        int[] data = {1, 4, 3, 2};
-        int el = 6;
-        int result = FindLoop.indexOf(data, el);
-        int expected = -1;
+    public void whenFind3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
         Assert.assertEquals(expected, result);
     }
 
     @Test
-    public void whenArrayHas8Then0() {
-        int[] data = {5, 6, 7, 8};
-        int el = 7;
-        int result = FindLoop.indexOf(data, el);
-        int expected = 2;
+    public void whenNotFind1() {
+        int[] data = new int[] {4, 6, 10, 3, 1};
+        int el = 1;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = -1;
         Assert.assertEquals(expected, result);
     }
+
 }
